4c2a23abec60efd48e63b1375d24edaef579646e
UncleBen420 <remyvu@outlook.com>
2022-08-20 16:37:50 +0200

    add the test

---
+++ test_test.py
@@ -0,0 +1,18 @@
+# content of test_class.py
+import pytest
+
+
+# declaring class
+class GfG:
+    name = "GeeksforGeeks"
+    age = 24
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        # initializing object
+        obj = GfG()
+        assert hasattr(obj, "age")
